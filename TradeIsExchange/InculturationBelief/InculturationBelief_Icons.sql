b912cb5131bb299c71d4b899936d0b8f484cd7d0
Vladyslav <arteezysolo@gmail.com>
2025-01-24 07:49:09 +0200

    upd

--- TradeIsExchange/InculturationBelief/InculturationBelief_Icons.sql
+++
@@ -1,7 +0,0 @@
--- TradeTweaksIcons
--- Author: MuXu
--- DateCreated: 5/6/2023 9:14:30 PM
---------------------------------------------------------------
-INSERT INTO IconDefinitions	
-		(Name,											Atlas, 										'Index')
-VALUES  ('ICON_BELIEF_LEU_INCULTURATION',				'ICON_ATLAS_BELIEFS_PATHEON',				25);
